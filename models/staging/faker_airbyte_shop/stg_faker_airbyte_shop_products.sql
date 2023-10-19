da6e60869c79cabd0da06495b1fee45e03bab1c7
jens-otto.moeller@coelacanth.cc <jens-otto.moeller@coelacanth.cc>
2023-10-19 07:39:40 +0000

    staging folder and files completed for all source tables: products, purchases, users.

--- models/staging/faker_airbyte_shop/stg_faker_airbyte_shop_products.sql
+++ models/staging/faker_airbyte_shop/stg_faker_airbyte_shop_products.sql
@@ -1,16 +1,34 @@
 with
-    source as (select * from {{ source('faker_airbyte', 'products') }}),
+source as (select * from {{ source("faker_airbyte", "products") }}),
 
-    renamed as (
+renamed as (
 
-        select
-            id as product_id,
-            model as car_model,
-            make as car_make,
-            price as price_usd
-        from source
+    select
 
-    )
+        -- ids
+        id as product_id,
+        year as model_year,
+
+        -- strings
+        _airbyte_raw_id as airbyte_unique_id,
+        model as car_model,
+        make as car_make,
+
+        -- numerics
+        price as price_usd,
+
+        -- booleans (there are no booleans in the source data)
+        -- dates (there are no dates in the source data)
+        -- timestamps
+        created_at as product_created_at,
+        updated_at as product_updated_at,
+        _airbyte_extracted_at as product_airbyte_extracted_at
+
+        -- json
+        -- the column '_airbyte_meta' is not extracted
+    from source
+
+)
 
 select *
 from renamed
---
+++ models/staging/faker_airbyte_shop/stg_faker_airbyte_shop_purchases.sql
@@ -0,0 +1,34 @@
+with
+    source as (select * from {{ source("faker_airbyte", "purchases") }}),
+
+    renamed as (
+
+        select
+
+            -- ids
+            id as purchase_id,
+            product_id,
+            user_id,
+
+            -- strings
+            _airbyte_raw_id as airbyte_unique_id,
+
+            -- numerics (there are no numerics in the source data)
+            -- booleans (there are no booleans in the source data)
+            -- dates (there are no dates in the source data)
+            -- timestamps
+            purchased_at,
+            returned_at as purchase_returned_at,
+            created_at as purchase_created_at,
+            updated_at as purchase_updated_at,
+            added_to_cart_at,
+            _airbyte_extracted_at as purchase_airbyte_extracted_at,
+
+        -- json
+        -- the column '_airbyte_meta' is not extracted
+        from source
+
+    )
+
+select *
+from renamed
