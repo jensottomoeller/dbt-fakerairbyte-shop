fb828ecf1e1b0c086fdf928528aebcb4c8e94af3
jens-otto.moeller@coelacanth.cc <jens-otto.moeller@coelacanth.cc>
2023-10-20 14:51:16 +0000

    updates to marts

--- models/staging/faker_airbyte_shop/stg_faker_airbyte_shop_users.sql
+++ models/staging/faker_airbyte_shop/stg_faker_airbyte_shop_users.sql
@@ -1,50 +1,51 @@
 with
-    source as (select * from {{ source("faker_airbyte", "users") }}),
+source as (select * from {{ source("faker_airbyte", "users") }}),
 
-    renamed as (
+renamed as (
 
-        select
+    select
 
-            -- ids
-            id as user_id,
+        -- ids
+        id as user_id,
 
-            -- strings
-            _airbyte_raw_id as airbyte_unique_id,
-            occupation,
-            gender,
-            academic_degree,
-            language,
-            telephone,
-            title,
-            nationality,
-            blood_type,
-            name,
-            email,
-            height,
+        -- strings
+        _airbyte_raw_id as airbyte_unique_id,
+        occupation,
+        gender,
+        academic_degree,
+        language,
+        telephone,
+        title,
+        nationality,
+        blood_type,
+        name,
+        email,
+        height,
 
-            -- numerics (there are no numerics in the source data)
-            -- booleans (there are no booleans in the source data)
-            -- dates (there are no dates in the source data)
-            -- timestamps
-            _airbyte_extracted_at as user_airbyte_extracted_at,
-            created_at as user_created_at,
-            updated_at as user_updated_at,
+        -- numerics (there are no numerics in the source data)
+        -- booleans (there are no booleans in the source data)
+        -- dates (there are no dates in the source data)
+        -- timestamps
+        _airbyte_extracted_at as user_airbyte_extracted_at,
+        created_at as user_created_at,
+        updated_at as user_updated_at,
 
-            -- json
-            string(json_query(address, '$.city')) as city,
-            string(json_query(address, '$.country_code')) as country_code,
-            cast(string(json_query(address, '$.postal_code')) as int64) as postal_code,
-            string(json_query(address, '$.province')) as province,
-            string(json_query(address, '$.state')) as state,
-            string(json_query(address, '$.street_name')) as street_name,
-            cast(
-                string(json_query(address, '$.street_number')) as int64
-            ) as street_number,
+        -- json
+        cast(string(json_query(address, '$.postal_code')) as int64)
+            as postal_code,
+        cast(
+            string(json_query(address, '$.street_number')) as int64
+        ) as street_number,
+        string(json_query(address, '$.city')) as city,
+        string(json_query(address, '$.country_code')) as country_code,
+        string(json_query(address, '$.province')) as province,
+        string(json_query(address, '$.state')) as state,
+        string(json_query(address, '$.street_name')) as street_name
 
         -- the column '_airbyte_meta' is not extracted
-        from source
+    from source
 
-    )
+)
 
 select *
 from renamed
