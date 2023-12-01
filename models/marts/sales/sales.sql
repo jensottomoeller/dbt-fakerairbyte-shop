713a5fab36990cf5f31e2715b27ab9a6f5b981c3
Jens Otto Moeller <jens-otto.moeller@coelacanth.cc>
2023-12-01 13:42:34 +0000

    - update all models with field _airbyte_extracted_at - added file dbt_run_on_merge.yml for GitHub Actions to create PROD on merge to main branch - create file run_and_monitor_dbt_job.py  or GitHub Actions to create PROD on merge to main branch (code copied 1:1 from https://gist.github.com/b-per/f4942acb8584638e3be363cb87769b48)

--- models/marts/sales/sales.sql
+++ models/marts/sales/sales.sql
@@ -27,18 +27,19 @@ users as (
 
 all_purchases as (
 
-select
-    purchases_purchase_id,
-    purchases_product_id,
-    purchases_user_id,
-    purchases_airbyte_unique_id,
-    purchases_purchased_at,
-    purchases_returned_at,
-    purchases_created_at,
-    purchases_updated_at,
-    purchases_added_to_cart_at,
+    select
+        purchases_purchase_id,
+        purchases_product_id,
+        purchases_user_id,
+        purchases_airbyte_unique_id,
+        purchases_airbyte_extracted_at,
+        purchases_purchased_at,
+        purchases_returned_at,
+        purchases_created_at,
+        purchases_updated_at,
+        purchases_added_to_cart_at
 
-from purchases
+    from purchases
 
 ),
 
@@ -46,17 +47,18 @@ from purchases
 
 all_products as (
 
-select
-    products_product_id,
-    products_model_year,
-    products_airbyte_unique_id,
-    products_car_model,
-    products_car_make,
-    products_price_usd,
-    products_created_at,
-    products_updated_at,
+    select
+        products_product_id,
+        products_model_year,
+        products_airbyte_unique_id,
+        products_airbyte_extracted_at,
+        products_car_model,
+        products_car_make,
+        products_price_usd,
+        products_created_at,
+        products_updated_at
 
-from products
+    from products
 
 ),
 
@@ -64,49 +66,48 @@ from products
 
 all_users as (
 
-select
-    users_user_id,
-    users_airbyte_unique_id,
-    users_occupation,
-    users_gender,
-    users_academic_degree,
-    users_language,
-    users_telephone,
-    users_title,
-    users_nationality,
-    users_blood_type,
-    users_name,
-    users_email,
-    users_height,
-    users_weight,
-    users_age,
-    users_airbyte_extracted_at,
-    users_created_at,
-    users_updated_at,
-    users_postal_code,
-    users_street_number,
-    users_city,
-    users_country_code,
-    users_province,
-    users_state,
-    users_street_name
+    select
+        users_user_id,
+        users_airbyte_unique_id,
+        users_airbyte_extracted_at,
+        users_occupation,
+        users_gender,
+        users_academic_degree,
+        users_language,
+        users_telephone,
+        users_title,
+        users_nationality,
+        users_blood_type,
+        users_name,
+        users_email,
+        users_height,
+        users_weight,
+        users_age,
+        users_created_at,
+        users_updated_at,
+        users_postal_code,
+        users_street_number,
+        users_city,
+        users_country_code,
+        users_province,
+        users_state,
+        users_street_name
 
-from users
+    from users
 ),
 
 -- STEP 3: FINAL CTE
 
 purchases_products_users_joined as (
 
-    select 
-        *
+    select *
     from all_purchases
 
     left join all_products
-    on all_purchases.purchases_product_id = all_products.products_product_id
-    
+        on all_purchases.purchases_product_id = all_products.products_product_id
+
     left join all_users
-    on all_purchases.purchases_user_id = all_users.users_user_id
+        on all_purchases.purchases_user_id = all_users.users_user_id
 
 )
 
